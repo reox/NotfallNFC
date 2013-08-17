9390360e067d8ede038f1a69877febf73b4a3007
Sebastian Bachmann <me@free-minds.net>
2013-08-17 10:37:42 +0200

    implementing a action bar for fast switch of views

--- Notfall/src/at/reox/emergency/EmergencyReaderActivity.java
+++ Notfall/src/at/reox/emergency/EmergencyReaderActivity.java
@@ -1,8 +1,12 @@
 package at.reox.emergency;
 
-import android.os.Bundle;
+import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.SpinnerAdapter;
 
 public class EmergencyReaderActivity extends Activity {
 
@@ -10,6 +14,25 @@ public class EmergencyReaderActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
 	super.onCreate(savedInstanceState);
 	setContentView(R.layout.activity_emergency_reader);
+
+	Context context = getActionBar().getThemedContext();
+
+	SpinnerAdapter mSpinnerAdapter = ArrayAdapter.createFromResource(context,
+	    R.array.readerOptions, android.R.layout.simple_spinner_dropdown_item);
+
+	getActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
+
+	getActionBar().setListNavigationCallbacks(mSpinnerAdapter,
+	    new ActionBar.OnNavigationListener() {
+
+		@Override
+		public boolean onNavigationItemSelected(int arg0, long arg1) {
+		    // TODO Auto-generated method stub
+		    return false;
+		}
+
+	    });
+
     }
 
     @Override
